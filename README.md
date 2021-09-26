07096cea5fb64dc5f5eb5d33e4d6fdb125963ebd
Masrur Hossain <masrur@compute.club>
2021-09-26 14:49:20 -0700

    API design

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# adb
+# Adobe API details and programming
+
+server.py is a simple http server
+
+adobe.drawio.pdf is architecture for the service
+
+repos.py prints repos sorted by date modified
---
+++ server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import http.server
+import socketserver
+from http import HTTPStatus
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(b'Hello Adobe!')
+
+
+httpd = socketserver.TCPServer(('', 8443), Handler)
+httpd.serve_forever()
